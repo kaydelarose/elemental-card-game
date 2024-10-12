d97988cbbc93fd097a4344e5d3439eb87bd56754
kaydelarose <kaydelarose@gmail.com>
2024-10-12 13:48:31 -0700

    Update Element.java

--- src/main/java/com/niantic/models/Element.java
+++ src/main/java/com/niantic/models/Element.java
@@ -12,24 +12,18 @@ public class Element {
     public static final String air = "Air";
     public static final String lightning = "Lightning";
 
-    // created random object to generate random elements
     private static final Random random = new Random();
 
-    // array holding all elements
+
     private static final String[] elements = {fire, water, earth, air, lightning};
 
-    // method used to randomly select an element
     public static String getRandomElement() {
-        return elements[random.nextInt(elements.length)]; // generating a random number between 0 and the length of elements
+        return elements[random.nextInt(elements.length)];
     }
 
-    // hashmap to store which element is stronger against other elements
-    // hashmap to store which element is weaker against other elements
     private static final Map<String, String> strongAgainst = new HashMap<>();
     private static final Map<String, String> weakAgainst = new HashMap<>();
 
-
-    // static block to initialize each map
     static {
         strongAgainst.put(fire, air);
         strongAgainst.put(water, fire);
@@ -45,22 +39,18 @@ public class Element {
 
     }
 
-    // return the element this one is strong against
     public static String getStrongAgainst(String element) {
         return strongAgainst.get(element);
     }
 
-    // return the element this one is weak against
     public static String getWeakAgainst(String element) {
         return weakAgainst.get(element);
     }
 
-    // method to check if element is strong against other element
     public static boolean isStrongAgainst(String element, String other) {
         return strongAgainst.get(element).equals(other);
     }
 
-    // method to check if element is weaker against other element
     public static boolean isWeakAgainst(String element, String other) {
         return weakAgainst.get(element).equals(other);
     }
